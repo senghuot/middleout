1c4815641eb0031d439e3c7cdc28556fbad04ba7
senghuot <senghuotlim@gmail.com>
2015-08-21 13:37:12 -0700

    combining files

--- ProductsApp/Controllers/UploadController.cs
+++ ProductsApp/Controllers/UploadController.cs
@@ -8,14 +8,15 @@ using System.Threading.Tasks;
 using System.Diagnostics;
 using System.Web;
 using System.IO;
+using System.Threading;
 
 namespace ProductsApp.Controllers {
     public class UploadController : ApiController {
-        
+        // to lock the thread while seeking file information
+        private static object locker = new Object();
    
         [HttpPost]
         public async Task<HttpResponseMessage> post() {
-            Trace.WriteLine("Hell Yeah");
             // check if the request contains multipart/form-data
             if (!Request.Content.IsMimeMultipartContent())
                 throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
@@ -27,16 +28,37 @@ namespace ProductsApp.Controllers {
                 var result      = await Request.Content.ReadAsMultipartAsync(provider);
                 var filename    = result.FormData["resumableFilename"];
                 var chunkNumber = result.FormData["resumableChunkNumber"];
-                var totalChunk  = result.FormData["resumableTotalChunk"];
+                var totalChunk  = result.FormData["resumableTotalChunks"];
+                
                 // This illustrates how to get the file names.
                 foreach (MultipartFileData file in provider.FileData) {
+                    string tmpFilename =  String.Format("{0}//{1}-{2}-{3}", root, chunkNumber, totalChunk, filename);
+                    System.IO.File.Move(file.LocalFileName, tmpFilename);
+                }
+                
+                // check if all the files had been saved
+                if (chunkNumber == totalChunk) {
+                    var loop = true;
+                    string[] files = null;
+                    while (loop) {
+                        files = System.IO.Directory.GetFiles(root);
+                        loop = !(files.Length == int.Parse(totalChunk));
+                        Thread.Sleep(2000);
+                    }
+                    Trace.WriteLine("All the chunks had been uploaded");
+                    var fs = new FileStream(root + "//" + filename, FileMode.CreateNew);
+                    var sortedfiles = files.OrderBy(n => n);
+                    foreach (var file in sortedfiles) {
+                        var buffer = System.IO.File.ReadAllBytes(file);
+                        fs.Write(buffer, 0, buffer.Length);
+                        System.IO.File.Delete(file);
+                    }
+                    fs.Close();
+                }
 
-                    Trace.WriteLine("We're here ");
-                }      
                 return Request.CreateResponse(HttpStatusCode.OK);
             } 
-            catch (System.Exception e) 
-            {
+            catch (System.Exception e) {
                 return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e);
             }
         }
